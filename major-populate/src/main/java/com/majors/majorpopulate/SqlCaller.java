24bd4fe6eb297b446665eb79c3ac925477a27a75
Curtis  Wilson <cww506@gmail.com>
2023-02-07 22:14:37 -0500

    -refactored Constants and Stuff. -moved sql connection to SqlCaller class -TODO refactor showMajorRequirements into sqlCaller

---
+++ major-populate/src/main/java/com/majors/majorpopulate/SqlCaller.java
@@ -0,0 +1,68 @@
+//This class will handle all of the calls to SQL.  eventually the url/username/password could be added to a settings file. Or taken as input and used for login purposes.
+// Place all calls to Join tables to filter SQL results here.
+
+package com.majors.majorpopulate;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class SqlCaller {
+    
+    Statement sqlSt;
+    ResultSet result;
+    
+    public SqlCaller(){
+        try{
+            Properties props = new Properties();
+            String dbSettingsPropertiesFile = "major-populate\\src\\main\\resources\\application.properties";
+            FileReader fr = new FileReader(dbSettingsPropertiesFile);
+            props.load(fr);
+
+            Class.forName(props.getProperty("spring.datasource.database"));
+            Connection dbConnect = DriverManager.getConnection(props.getProperty("spring.datasource.url"), 
+            props.getProperty("spring.datasource.username"), props.getProperty("spring.datasource.password"));
+            sqlSt = dbConnect.createStatement();
+            
+        }catch(ClassNotFoundException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Class not Found, Check the JAR");
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("SQL IS BAD!!" + ex.getMessage());
+
+        }catch (FileNotFoundException ex){
+            //fdafdas
+        }catch (Exception exception) {}
+        
+    }
+
+    private void getConnection(){
+        
+    }
+
+    public ResultSet GetMajors(){
+        String SQL = "SELECT * FROM tbl_majors";
+        try{
+            result = sqlSt.executeQuery(SQL);
+        }catch(SQLException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("SQL IS BAD!!" + ex.getMessage());
+        }
+        return result;
+    }
+    /* public ResultSet ShowMajorRequirementSet(){
+        String SQLMajors = "SELECT * FROM tbl_grad_requirement WHERE major_name = '" + nameOfMajor + "'";
+        String SQLMajorElectives = "SELECT * FROM tbl_major_electives WHERE major_name = '" + nameOfMajor + "'";
+
+    } */
+}
