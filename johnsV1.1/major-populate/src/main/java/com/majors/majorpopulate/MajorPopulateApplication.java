324d7250fdc725b6d93242236ceef4e1bfa651f3
Stephen Prine <95665904+Stephen-Prine@users.noreply.github.com>
2023-02-13 13:01:28 -0500

    Add files via upload

---
+++ johnsV1.1/major-populate/src/main/java/com/majors/majorpopulate/MajorPopulateApplication.java
@@ -0,0 +1,13 @@
+package com.majors.majorpopulate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorPopulateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorPopulateApplication.class, args);
+	}
+
+}
