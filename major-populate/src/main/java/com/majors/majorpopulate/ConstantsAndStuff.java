237ba6691046b6580f777dd3a1f281342ee1ba9e
Stephen Prine <Starvingandrew@gmail.com>
2023-02-21 09:47:41 -0500

    fixed *fingers crossed

--- major-populate/src/main/java/com/majors/majorpopulate/ConstantsAndStuff.java
+++ major-populate/src/main/java/com/majors/majorpopulate/ConstantsAndStuff.java
@@ -3,6 +3,9 @@ package com.majors.majorpopulate;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,31 +13,33 @@ import java.util.List;
 
 public class ConstantsAndStuff {
     
-    //make single connection to SQL. SqlCaller Class.  
+    //make single connection to SQL. SqlCaller Class.
+    //@Service 
     public static SqlCaller sql = new SqlCaller();
     public static List<String> majorList;
-    
+    public static List<Login> loggedInUser = new ArrayList<>();
+
     public ConstantsAndStuff(){}
        
     //adds all the majors to a list to add to the dropdown Select option on form.html
     public static void populateMajorChoices() throws Exception{ 
-        majorList = new ArrayList<>();        
-        
+        List<Major> totalMajorList = new ArrayList<>();
+        //totalMajorList = sql.getAllMajors();
         majorList = sql.ShowMajorNames();
+        System.out.println(majorList);
         
     }
 
+    
+
+    
+
     //adds the major requirements and course id to the mainpage.html
-     public static void showMajorRequirements(String nameOfMajor) throws Exception{
+     public static Major showMajorRequirements(String MajorId) throws Exception{
             
-        
-             List<MajorRequirements> result = sql.ShowMajorRequirementSet();
-            
-             for (MajorRequirements majorRequirements : result) {
-                
-             }
-               
-                //majorElectives.add(new MajorElectives(result.getString("major_name"), result.getString("elective_group"), result.getString("nbr_required")));   
+             Major major = sql.GetMajorById(MajorId);
+             return major;
+                  
     }
     
       public static void CreateStudent(Student student){
@@ -59,6 +64,104 @@ public class ConstantsAndStuff {
             System.out.println("SQL IS BAD!!" + ex.getMessage());
 
         }
+    }
+
+    //gets credentials from database to see if there is user
+    public static String doesCredentialsMatch(String name, String password) {
+        java.sql.Statement sqlSt; //runs sql
+        
+        ResultSet result; //holds the output from the sql
+        
+        String SQL = "SELECT count(id) FROM tbl_student where name = '" + name + "' and password = '" + password + "'";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String dbURL = "jdbc:mysql://127.0.0.1:3306/cpt275_db";
+            Connection dbConnect = DriverManager.getConnection(dbURL, "root", "root");
+            sqlSt = dbConnect.createStatement(); //allows SQL to be executed
+            result = sqlSt.executeQuery(SQL);
+            while(result.next() != false) { 
+                if(result.getString("count(id)").equals("1")){
+                    if (loggedInUser.size() == 1){
+                        loggedInUser.set(0, new Login(name, password));
+                    } else {
+                        loggedInUser.add(new Login(name, password));
+                    }
+                }
+             return result.getString("count(id)");
+            }
+            sqlSt.close();
+            
+        }catch(ClassNotFoundException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Class not Found, Check the JAR");
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("SQL IS BAD!!" + ex.getMessage());
+            
+        }
+        return "No Course Name";
+       
+    }
+
+    //gets major from logged in user
+    public static String getMajorNameFromLoggedInUser(String name, String password){
+        java.sql.Statement sqlSt; //runs sql
+        
+        ResultSet result; //holds the output from the sql
+        
+        String SQL = "SELECT major_name FROM tbl_student where name = '" + name + "' and password = '" + password + "'";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String dbURL = "jdbc:mysql://127.0.0.1:3306/cpt275_db";
+            Connection dbConnect = DriverManager.getConnection(dbURL, "root", "root");
+            sqlSt = dbConnect.createStatement(); //allows SQL to be executed
+            result = sqlSt.executeQuery(SQL);
+            while(result.next() != false) {
+             return result.getString("major_name");
+            }
+            sqlSt.close();
+            
+        }catch(ClassNotFoundException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Class not Found, Check the JAR");
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("SQL IS BAD!!" + ex.getMessage());
+            
+        }
+        return "No Course Name";
+    }
+
+    //gets the major ID from the name of major
+    public static String getMajorIdFromName(String majorName){
+        java.sql.Statement sqlSt; //runs sql
+        
+        ResultSet result; //holds the output from the sql
+        
+        String SQL = "SELECT major_id FROM tbl.majors where major_name = '" + majorName +"'";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String dbURL = "jdbc:mysql://127.0.0.1:3306/cpt275_db";
+            Connection dbConnect = DriverManager.getConnection(dbURL, "root", "root");
+            sqlSt = dbConnect.createStatement(); //allows SQL to be executed
+            result = sqlSt.executeQuery(SQL);
+            while(result.next() != false) {
+             return result.getString("major_id");
+            }
+            sqlSt.close();
+            
+        }catch(ClassNotFoundException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Class not Found, Check the JAR");
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(MajorPopulateApplication.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("SQL IS BAD!!" + ex.getMessage());
+            
+        }
+        return "No Course Name";
     }
 
 
