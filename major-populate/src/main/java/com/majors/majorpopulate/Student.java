2a5b38e07d8558fbd4b9fb20380418117d98017d
John Percival <johnpercival@my.tridenttech.edu>
2023-02-28 17:45:33 -0500

    updated

--- major-populate/src/main/java/com/majors/majorpopulate/Student.java
+++ major-populate/src/main/java/com/majors/majorpopulate/Student.java
@@ -3,22 +3,33 @@ package com.majors.majorpopulate;
 import jakarta.validation.constraints.NotBlank;
 
 public class Student {
-    @NotBlank (message = "Cannot Be Blank")
+
+    private int studentId;
+    @NotBlank(message = "Cannot Be Blank")
     private String name;
-    @NotBlank (message = "Cannot Be Blank")
+    @NotBlank(message = "Cannot Be Blank")
     private String password;
-    @NotBlank (message = "Cannot Be Blank")
+    @NotBlank(message = "Cannot Be Blank")
     private String passwordValidation;
-    @NotBlank (message = "Cannot Be Blank")
+    @NotBlank(message = "Cannot Be Blank")
     private String major;
-     
-    public Student()
-    {}
+
+    public Student() {
+    }
+
     public Student(String name, String password, String passwordValidation, String major) {
         this.name = name;
         this.password = password;
         this.passwordValidation = passwordValidation;
         this.major = major;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
     }
 
     public String getName() {
@@ -52,7 +63,7 @@ public class Student {
     public String getPasswordValidation() {
         return passwordValidation;
     }
-    
+
     @Override
     public String toString() {
         return "Student{" +
