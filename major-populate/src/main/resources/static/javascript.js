f8c8f55b73209a805a7a1ae60ec4009fb731cd67
Stephen Prine <Starvingandrew@gmail.com>
2023-03-07 12:53:26 -0500

    css/javascript to mainpage.html
    added some css and javascript functionality to mainpage.html

--- major-populate/src/main/resources/static/javascript.js
+++ major-populate/src/main/resources/static/javascript.js
@@ -9,3 +9,15 @@ function showElect() {
     electDiv.classList.toggle("toggleElectives");
     
 };
+
+function coreInfoClick(courseName) {
+    document.querySelector('.information').classList.toggle("show");
+    document.querySelector('.information').innerHTML = `<strong> ${courseName}</strong> <br> 
+        This class name is ${courseName} <br> and a description for a ${courseName} goes here<br>
+        ....PreReqExAmp101 min grade: C <br>
+        ....PreReqExAmp102 min grade: C `; 
+}
+
+function electShowClick(){
+    document.querySelector('.electiveChoices').classList.toggle("show");
+}
